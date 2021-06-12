449ed5b569b58161b0a79a3026f1777c4228375f
jcontti <conttijorge@gmail.com>
2021-06-12 20:04:00 -0300

    k8s agent docker-slave is working

--- delete-cluster.sh
+++ delete-cluster.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
 helm uninstall jenkins -n jenkins 
-
 kind delete cluster --name k8s-cluster-1 
-
-#kubectl delete pv -n jenkins jenkins-pv
-#kubectl delete sa -n jenkins jenkins
-
-#kubectl delete all --all -n jenkins
-
