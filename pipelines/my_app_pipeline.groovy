e2059c459d6fd6740e548f9679ed1748040a9576
jcontti <conttijorge@gmail.com>
2021-06-12 20:10:47 -0300

    pipeline fix

--- pipelines/my_app_pipeline.groovy
+++ pipelines/my_app_pipeline.groovy
@@ -1,51 +1,47 @@
-/* pipeline {
-  agent any
-  stages {
-    stage('Stage 1') {
-      steps {
-        echo 'Hello world!'
-      }
-    }
-  }
-} */
+def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
 
-pipeline {
-  agent {
-    label {
-      label "docker-slave"
-    }
-  }
-  environment {
-    GITHUB_PROJECT = 'https://github.com/guipal/apiSampleJava'
-  }
-  stages {
-    stage('Checkout project git') {
-      steps {			
-        git credentialsId: 'GHsvcAccnt', url: "${GITHUB_PROJECT}.git"
-      }
-    }
-    stage('Build docker image') {
-      steps {
-        container('dind') {
-          script {
-              image = docker.build(dockerImage, "${pipelineParams.buildArgs} .")
-          }
+    pipeline {
+      agent {
+        label {
+          label "docker-slave"
         }
       }
-    }
-/* 			stage('Push docker image') {
-      steps {
-        container('dind') {
-          withAWS(region: config.region, credentials: config.awsCred) {
-            sh ecrLogin()
-            script {
-              random_suffix = utils.getRandomString(5)
-              image.push("${BUILD_NUMBER}-${random_suffix}")
-              image.push('latest')
+      environment {
+        GITHUB_PROJECT = 'https://github.com/guipal/apiSampleJava'
+      }
+      stages {
+        stage('Checkout project git') {
+          steps {			
+            git credentialsId: 'GHsvcAccnt', url: "${GITHUB_PROJECT}.git"
+          }
+        }
+        stage('Build docker image') {
+          steps {
+            container('dind') {
+              script {
+                  image = docker.build(dockerImage, "${pipelineParams.buildArgs} .")
+              }
             }
           }
         }
+    /* 			stage('Push docker image') {
+          steps {
+            container('dind') {
+              withAWS(region: config.region, credentials: config.awsCred) {
+                sh ecrLogin()
+                script {
+                  random_suffix = utils.getRandomString(5)
+                  image.push("${BUILD_NUMBER}-${random_suffix}")
+                  image.push('latest')
+                }
+              }
+            }
+          }
+        } */
       }
-    } */
-  }
+    }
 }
